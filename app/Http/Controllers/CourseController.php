57d080f4e07d7012f31d9e2217c4ecc794fe334f
zhafri-shafiq <skyisthelimit175@ymail.com>
2017-03-01 10:19:49 +0000

    second commit

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -12,25 +12,65 @@ class CourseController extends Controller
 	public function index()
 	{
 		// $courses = Course::all();
+
+		//try with natural join
+
 		$courses["data"] = [
-			DB::select("SELECT admin_coursemaker_courses.name, admin_coursemaker_courses.description, system_files.attachment_id, system_files.attachment_type FROM admin_coursemaker_courses, system_files WHERE system_files.attachment_id = admin_coursemaker_courses.id AND system_files.attachment_type LIKE '%course';"),
-
-
-			// DB::select('SELECT file_name FROM system_files')
-			
+			DB::select(
+				"SELECT admin_coursemaker_courses.name, admin_coursemaker_courses.description, system_files.file_name
+				FROM admin_coursemaker_courses, system_files 
+				WHERE system_files.attachment_id = admin_coursemaker_courses.id 
+				AND system_files.attachment_type LIKE '%course';"),
 		];
+
 		
+
 		return response()->json($courses);
+
+
+
+		/* 
+		$courses["data"] = [
+			DB::select(
+				"SELECT admin_coursemaker_courses.name, admin_coursemaker_courses.description, system_files.attachment_id, system_files.attachment_type 
+				FROM admin_coursemaker_courses, system_files 
+				WHERE system_files.attachment_id = admin_coursemaker_courses.id 
+				AND system_files.attachment_type LIKE '%course';"),
+		];
+
+		return response()->json($courses);
+
+		*/
+
+
+
+		//mus ajar
+
+		// $courses = 
+		// 	json_encode(DB::select(
+		// 		"SELECT admin_coursemaker_courses.name, admin_coursemaker_courses.description, system_files.attachment_id, system_files.attachment_type 
+		// 		FROM admin_coursemaker_courses, system_files 
+		// 		WHERE system_files.attachment_id = admin_coursemaker_courses.id 
+		// 		AND system_files.attachment_type LIKE '%course';"));
+
+
+		// 	 //DB::select('SELECT file_name FROM system_files')
+		// 	var_dump($courses);
+		
+		// $courses2["title"] = $courses['name'];
+
+		// return response()->json($courses2);
 	}
 
 	public function getCourse($id)
 	{
 		// $course = Course::find($id);
-		$course["data"] = [
-			DB::select("SELECT admin_coursemaker_lessons.name FROM admin_coursemaker_lessons WHERE admin_coursemaker_lessons.course_id = $id;"),
-		];
+		$course = 
+			json_encode(DB::select("SELECT admin_coursemaker_lessons.name FROM admin_coursemaker_lessons WHERE admin_coursemaker_lessons.course_id = $id;"));
 
-		return response()-> json($course);
+		var_dump($course);
+		$course2["title"] = $course["name"];
+		return response()-> json($course2);
 	}
 
 	public function saveCourse(Request $request)
